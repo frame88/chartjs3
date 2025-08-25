de63734336c54ad14a08519a5b9c474187c9a086
francesco.leuzzi <leuzzibiz@gmail.com>
2025-08-25 13:53:26 +0200

    versione con i Signal, quindi Effects

--- src/app/shared/chartjs.component.ts
+++ src/app/shared/chartjs.component.ts
@@ -1,39 +1,38 @@
-import { AfterViewInit, Component } from '@angular/core';
+import { AfterViewInit, Component, effect, ElementRef, viewChild } from '@angular/core';
 import { Chart } from 'chart.js/auto';
 
 @Component({
   selector: 'app-chartjs',
   imports: [],
   template: `
-    <canvas id="myChart"></canvas>
+    <canvas #myChart></canvas>
   `,
   styles: ``
 })
-export class ChartjsComponent implements AfterViewInit{
+export class ChartjsComponent {
+
+  canvas = viewChild<ElementRef<HTMLCanvasElement>>('myChart');
 
   constructor() {
-  }
-  
-  ngAfterViewInit(): void {
-    const ctx = document.getElementById('myChart') as HTMLCanvasElement;
-  
-    new Chart(ctx, {
-      type: 'bar',
-      data: {
-        labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
-        datasets: [{
-          label: '# of Votes',
-          data: [12, 19, 3, 5, 2, 3],
-          borderWidth: 1
-        }]
-      },
-      options: {
-        scales: {
-          y: {
-            beginAtZero: true
+    effect(() => {  
+      new Chart(this.canvas()!.nativeElement, {
+        type: 'bar',
+        data: {
+          labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+          datasets: [{
+            label: '# of Votes',
+            data: [12, 19, 3, 5, 2, 3],
+            borderWidth: 1
+          }]
+        },
+        options: {
+          scales: {
+            y: {
+              beginAtZero: true
+            }
           }
         }
-      }
+      });
     });
   }
 }
